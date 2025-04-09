5abcdfe8b6088700e47154e98ce546563502097b
Jimmy Larsson <jimmy.w.larsson@gmail.com>
2025-04-09 07:17:04 +0200

    implement DateIterator and tests

--- src/fetch/period.rs
+++ src/fetch/period.rs
@@ -1,8 +1,48 @@
+use crate::fetch::DateHelper;
+
+use chrono::{Datelike, NaiveDate};
+use clap::Subcommand;
+
 use std::fmt::Display;
 use std::str::FromStr;
 
-use chrono::NaiveDate;
-use clap::Subcommand;
+#[derive(Debug)]
+pub struct DateIterator {
+    next_date: Option<NaiveDate>,
+    end_date: NaiveDate,
+    period: Period,
+}
+
+impl From<Period> for DateIterator {
+    fn from(value: Period) -> Self {
+        let start_date = value.start_date();
+        let end_date = value.end_date().unwrap_or(start_date);
+
+        DateIterator {
+            next_date: Some(start_date),
+            end_date,
+            period: value,
+        }
+    }
+}
+
+impl Iterator for DateIterator {
+    type Item = NaiveDate;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let current_date = self.next_date.take()?;
+
+        if current_date < self.end_date {
+            let next_date = current_date
+                .add_date_from_period(&self.period)
+                .expect("date out of bounds");
+
+            self.next_date = Some(next_date);
+        }
+
+        Some(current_date)
+    }
+}
 
 /// Period of the fetched file.
 /// Format:
@@ -19,6 +59,7 @@ pub enum Period {
         #[arg(short)]
         end_date: Option<NaiveDate>,
     },
+
     /// Fetch file(s) for each month in the period from start to end date.
     Monthly {
         /// Select the first date you want data from.
@@ -31,7 +72,8 @@ pub enum Period {
     },
 }
 
-fn parse_monthly(input: &str) -> Result<NaiveDate, &'static str> {
+//TODO: Create better parsing for period so start_date >= end_date etc
+pub fn parse_monthly(input: &str) -> Result<NaiveDate, &'static str> {
     let date = match NaiveDate::from_str(input) {
         Ok(date) => date,
         Err(_) => {
@@ -44,6 +86,30 @@ fn parse_monthly(input: &str) -> Result<NaiveDate, &'static str> {
 }
 
 impl Period {
+    #[allow(unused)]
+    pub fn new(start_date: NaiveDate, end_date: Option<NaiveDate>, period: PeriodName) -> Self {
+        match period {
+            PeriodName::Daily => {
+                let end_date = end_date.unwrap_or(start_date);
+                Self::Daily {
+                    start_date,
+                    end_date: Some(end_date),
+                }
+            }
+            PeriodName::Monthly => {
+                let end_date = end_date
+                    .unwrap_or(start_date)
+                    .with_day(1)
+                    .expect("1st is valid date");
+                let start_date = start_date.with_day(1).expect("1st is valid date");
+                Self::Monthly {
+                    start_date,
+                    end_date: Some(end_date),
+                }
+            }
+        }
+    }
+
     #[inline]
     pub fn period_name(&self) -> PeriodName {
         match self {
@@ -85,5 +151,53 @@ impl Display for PeriodName {
             PeriodName::Monthly => "monthly",
         };
         write!(f, "{}", s)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    fn nd(year: i32, month: u32, day: u32) -> NaiveDate {
+        NaiveDate::from_ymd_opt(year, month, day).unwrap()
+    }
+
+    use super::*;
+    #[test]
+    fn date_iter_daily() {
+        let period = Period::new(nd(2025, 1, 1), Some(nd(2025,1,5)), PeriodName::Daily);
+
+        let mut date_iter = DateIterator::from(period.clone());
+        assert_eq!(Some(nd(2025, 1, 1)), date_iter.next());
+        assert_eq!(Some(nd(2025, 1, 2)), date_iter.next());
+        assert_eq!(Some(nd(2025, 1, 3)), date_iter.next());
+        assert_eq!(Some(nd(2025, 1, 4)), date_iter.next());
+        assert_eq!(Some(nd(2025, 1, 5)), date_iter.next());
+        assert_eq!(None, date_iter.next());
+
+        let period = Period::new(nd(2025, 1, 1), None, PeriodName::Daily);
+        let mut date_iter = DateIterator::from(period.clone());
+        assert_eq!(Some(nd(2025, 1, 1)), date_iter.next());
+        assert_eq!(None, date_iter.next());
+        assert_eq!(None, date_iter.next());
+    }
+
+    #[test]
+    fn date_iter_monthly() {
+        let period = Period::new(nd(2025, 1, 1), Some(nd(2025, 3, 6)), PeriodName::Monthly);
+
+        println!("{period:?}");
+
+        let mut date_iter = DateIterator::from(period.clone());
+
+        assert_eq!(Some(nd(2025, 1, 1)), date_iter.next());
+        assert_eq!(Some(nd(2025, 2, 1)), date_iter.next());
+        assert_eq!(Some(nd(2025, 3, 1)), date_iter.next());
+        assert_eq!(None, date_iter.next());
+
+        let period = Period::new(nd(2025, 1, 1), Some(nd(2025, 1, 1)), PeriodName::Monthly);
+        let mut date_iter = DateIterator::from(period.clone());
+        assert_eq!(Some(nd(2025, 1, 1)), date_iter.next());
+        assert_eq!(None, date_iter.next());
+        assert_eq!(None, date_iter.next());
     }
 }
